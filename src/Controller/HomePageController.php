8cee3a92965d31e83c8668673782ed36b89f2df5
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-08-05 11:39:51 +0200

    maj

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,15 +11,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
 {
-    #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, ): Response
+    #[Route('/', name: 'app_home_page', methods: ['GET'])]// methode Get on demande des infos au serveur et post pour envoyer des demandes au serveurs
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
-        $product = $productRepository->findAll();
         return $this->render('home_page/homePage.html.twig', [
             'controller_name' => 'HomePageController',
-            'products'=>$product
+            'products' => $productRepository -> findAll(),
+            'categories' => $categoryRepository -> findAll()
         ]);
     }
+
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]// methode Get on demande des infos au serveur et post pour envoyer des demandes au serveurs
+    public function showProduct(Product $product, ProductRepository $productRepository): Response
+    {
+        $lastProductsAdd = $productRepository->findBy([], ['id'=>'DESC'],5); //1ere condition : aucun filtrage pour récuperer tous les produtis, le 2eme : est pour classer les résultats par l'id par l'ordre décroissant et le 3ème argument c'est pour demander de nous retourner que 5 résultats
+
+        return $this->render('home_page/show.html.twig', [
+            'product' => $product,
+            'products' =>$lastProductsAdd
+        ]);
+    }
+
 }
 
             
