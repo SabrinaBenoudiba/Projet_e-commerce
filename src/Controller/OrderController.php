d50dbd6e7ff3e3a0cebd6dd5d53c57eef492d3fc
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-08-06 16:26:03 +0200

    maj OrderController

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\City;
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+final class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'app_order')]
+    public function index(ProductRepository $productRepository, Session $session, Request $request): Response
+    {
+        $cart = $session->get('cart', []);
+        $cartWithData = [];
+
+        foreach ($cart as $id => $quantity) {
+          
+            $cartWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity'=> $quantity
+            ];
+        }
+      
+        $total = array_sum(array_map(function ($item){
+             return $item['product']->getPrice() * $item['quantity'];
+        }, $cartWithData));
+        $order = New Order();
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        return $this->render('order/index.html.twig', [
+            'form'=>$form->createView(),
+            'total' => $total
+        ]);
+    }
+
+    #[Route('/city/{id}/shipping/cost', name: 'app_city_shipping_cost')]
+    public function cityShippingCost(City $city) : Response
+    {
+        $cityShippingPrice = $city->getShippingCost();
+
+        return new Response($cityShippingPrice);
+    }
+}
