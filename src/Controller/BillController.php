54fcd5ffdfe74dec9b92e7df3cea6fbf3c82396c
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-08-18 16:15:01 +0200

    update billController bill/index

--- src/Controller/BillController.php
+++ src/Controller/BillController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Repository\OrderRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BillController extends AbstractController
 {
-    #[Route('editor/order/{id}/bill', name: 'app_bill')]
+    #[Route('editor/orders/{id}/bill', name: 'app_bill')]
     public function index($id, OrderRepository $orderRepository): Response
     {
         $order = $orderRepository->find($id);
 
-        return $this->render('bill/index.html.twig', [
-            'order' => 'orders',
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont','Arial');
+        $domPdf = new Dompdf($pdfOptions);
+        $html = $this->renderView('bill/index.html.twig', [
+            'order'=>$order,
+        ]);
+        $domPdf->loadHtml($html);
+        $domPdf->render();
+        $domPdf->stream('bill-'.$order->getId().'.pdf',[
+            'Attachment'=>true
+        ]);
+
+        return new Response('',200,[ //
+            'Content-Type' => 'application/pdf' 
         ]);
     }
 }
